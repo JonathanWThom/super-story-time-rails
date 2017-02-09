1628116da94cc037c84bd0077a8a668c3e556e46
Jim Padilla and Jonathan Thom <jim+jonathan@>
2017-02-09 15:42:49 -0800

    Add admin, set global user

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,30 @@
+class AdminsController < ApplicationController
+  def welcome
+  end
+
+  def authenticate
+    user = User.find_by(name: authentication_params[:name])
+    if user && user.name = 'Admin'
+      user_logged_in = user.authenticate(authentication_params[:password])
+      if user_logged_in
+        session[:current_user_id] = user_logged_in.id
+        redirect_to admin_console_path
+      else
+        flash[:notice] = "Incorrect password"
+        redirect_to admin_path
+      end
+    else
+      flash[:notice] = "You're not an admin ya foo!"
+      redirect_to admin_path
+    end
+  end
+
+  def console
+
+  end
+
+private
+  def authentication_params
+    params.permit(:name, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'admin', to: 'admins#welcome'
+  get 'admin/log_in', to: 'admins#authenticate'
+  get 'admin/console', to: 'admins#console'
+
   get 'users/log_in', to: 'users#authenticate'
   get 'users/log_out', to: 'users#logout'
   resources :users do
